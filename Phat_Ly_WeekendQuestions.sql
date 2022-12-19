45a72d0c4e19e7c4ece9e0f78d19870fdb8866e1
phatly95 <120554369+phatly95@users.noreply.github.com>
2022-12-18 17:39:20 -0800

    Add files via upload

--- Phat_Ly_WeekendQuestions.sql
+++ Phat_Ly_WeekendQuestions.sql
@@ -74,7 +74,8 @@ SELECT
         cd.zip_cd,
         CASE 
             WHEN stat_cd !=  state_cd THEN 'data issues'
-            WHEN stat_cd IS NULL AND state_cd IS NULL THEN 'data issues'
+            WHEN stat_cd IS NULL THEN 'data issues'
+            WHEN state_cd IS NULL THEN 'data issues'
             ELSE 'good data'
         END AS stae_cd_status
 FROM cards_ingest.tran_fact AS tf
