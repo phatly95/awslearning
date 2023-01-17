07e5ff9253a8dc93e06c790cea57a61eade813e4
phatly95 <120554369+phatly95@users.noreply.github.com>
2023-01-17 02:06:15 -0800

    Add files via upload

---
+++ redshift/config/db_details.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+redshift_endpoint = "redshift-cluster-1.cklkmg9bcvlf.us-west-2.redshift.amazonaws.com"
+redshift_user = "phat_ly95"
+redshift_pass = "Blackmoon1995!"
+port = 5439
+dbname = "dev"
+
